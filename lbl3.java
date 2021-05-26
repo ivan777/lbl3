bd152a6fce3ac623da99708449e969e7d9b91a1b
ivan777 <privalov-iv@yandex.ru>
2021-05-26 23:49:12 +0500

    1.0.1

--- lbl3.java
+++ lbl3.java
@@ -1,19 +1,3 @@
-//public class HelloWorld {
-//    public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-//    }
-//}
-
-//"Hello, World!1"
-
-//class HelloWorld {
-//    public static void main(String[] args) {
-//        System.out.println("Привет, мир!");
-//        int i = 125215+49546;
-//        System.out.println(i);
-//    }
-//}
-
 
 import java.io.File;
 import java.io.FileNotFoundException;
