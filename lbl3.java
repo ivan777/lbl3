bee7ef7d727d7b04fcb7670befead777ea6021b0
ivan777 <privalov-iv@yandex.ru>
2021-06-02 00:03:11 +0500

    1.0.2

--- lbl3.java
+++ lbl3.java
@@ -12,34 +12,14 @@ public class lbl3 {
 //       String route = "C:\\Users\\privalovie\\YandexDisk\\Универ\\Конструирование\\labs\\lbl2.txt", str, str1;
 //         Graph Gor Graph12
         String route = "F:\\YandexDisk\\Универ\\Конструирование\\labs\\lbl2.txt", str, str1;
-        int zz=0,i1 = 0,A = 0,B = 0,C = 0,D = 0;
-        double summj1 = 0.0, summj2 = 0.0, summj3 = 0.0, summj4 = 0.0, summj5 = 0.0, summj5_5 = 0.0; //общее число ошибок в программе
+        int A = 0,B = 0,C = 0,D = 0;
+        double lamda = 1.53; //уровень языка программирования
         int[] array;
         double[][] data;
 
         try
         {
-            FileReader finkol = new FileReader(route);
-            Scanner src1 = new Scanner(finkol); // Чтение из файла
-
-            while (src1.hasNext()) {str1 = src1.nextLine(); zz++;}
-
-            finkol.close();
-            FileReader fin = new FileReader(route);
-            Scanner src = new Scanner(fin); // Чтение из файла
-
-            array = new int[zz];
-            data = new double[zz][6];
-
-            while (src.hasNext()) {
-                str = src.nextLine();
-                if(str.length()!=0) {
-                    array[i1] = Integer.parseInt(str);
-                    i1++;}
-            }
-            fin.close();
-
-            System.out.println("ВВедите значения переменных: ");
+            System.out.println("Введите значения переменных: ");
             Scanner inA = new Scanner(System.in);
             System.out.print("A: ");
             A = inA.nextInt();
@@ -56,26 +36,20 @@ public class lbl3 {
             int z = array.length - 1; // Кол-во ошибок
             lobject l1 = new lobject();
             l1.ParamВ = 31.21; // В
-            for (int j = 0; j <= z; j++) {
-                int m = array[j], num = j+1;
-                double Param5 = (num);
-                data[j][0] = num; //Номер ошибки (i)
-                data[j][1] = m; //Интервал  между ошибками (Xi, дни)
-                data[j][2] = num*m; //i*Xi
-                data[j][3] = 1/(l1.ParamВ-num+1); //1/B-i+1
-                data[j][4] = (l1.ParamВ-num+1)*m; //(B-i+1)*Xi
-                data[j][5] = 1/Param5; //1/i
 
-                //Суммирование
-                summj1 = summj1 + data[j][1]; //Интервал  между ошибками (Xi, дни)
-                summj2 = summj2 + data[j][2]; //i*Xi
-                summj3 = summj3 + data[j][3]; //1/B-i+1
-                summj4 = summj4 + data[j][4]; //(B-i+1)*Xi
-                summj5 = summj5 + data[j][5]; //1/i
-                if (num<=5) {
-                        summj5_5 = summj5_5 + data[j][5]; //1/i
-                    }
-                }
+            l1.EiXi = summj2;//E(i*Xi)
+            l1.EXi = summj1;//E(Xi)
+            l1.nEXi = zz * summj1;//n * E(Xi)
+            l1.EBi1Xi = summj4;//E((B-i+1)*Xi)
+            l1.Bn = l1.ParamВ - zz;//B - n
+            l1.B1EXi = (l1.ParamВ + 1)*summj1;//(B+1) * E(Xi)
+            l1.Fb1 = summj3;//F(b1)
+            l1.Fb2 = l1.nEXi/(l1.B1EXi-l1.EiXi);//F(b2)
+            l1.Fb1Fb2 = l1.Fb1 - l1.Fb2;//F(b1) - F(b2)
+            l1.K = zz/l1.EBi1Xi;//K
+            l1.Xn1 = 1/(l1.K*(l1.ParamВ - zz));//Xn+1
+            l1.tk = (1/l1.K)*(summj5_5);//tk
+
 
             System.out.println("============================================");
             System.out.println("============================================");
@@ -105,25 +79,10 @@ public class lbl3 {
                 System.out.println();
             }
 
-            l1.EiXi = summj2;//E(i*Xi)
-            l1.EXi = summj1;//E(Xi)
-            l1.nEXi = zz * summj1;//n * E(Xi)
-            l1.EBi1Xi = summj4;//E((B-i+1)*Xi)
-            l1.Bn = l1.ParamВ - zz;//B - n
-            l1.B1EXi = (l1.ParamВ + 1)*summj1;//(B+1) * E(Xi)
-            l1.Fb1 = summj3;//F(b1)
-            l1.Fb2 = l1.nEXi/(l1.B1EXi-l1.EiXi);//F(b2)
-            l1.Fb1Fb2 = l1.Fb1 - l1.Fb2;//F(b1) - F(b2)
-            l1.K = zz/l1.EBi1Xi;//K
-            l1.Xn1 = 1/(l1.K*(l1.ParamВ - zz));//Xn+1
-            l1.tk = (1/l1.K)*(summj5_5);//tk
-
             long stop_time = System.currentTimeMillis();
             long run_time = stop_time - start_time;
             output(l1,run_time);
-
         }
-
         catch(IOException e)
         {
             e.printStackTrace();
@@ -131,7 +90,7 @@ public class lbl3 {
     }
 
     public static class lobject implements Cloneable
-    {double ParamВ, EiXi, EXi, nEXi, EBi1Xi, Bn, B1EXi, Fb1, Fb2, Fb1Fb2, K, Xn1, tk;}
+    {double n2_1, V_1, B_1, K_2, I_2, K_2, N_2, V_2, P_2, TK_2, B_2, TN_2, M_2, V_2;}
 
     public static void output(lobject l1, long run_time)
     {
